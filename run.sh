a97d77b755286c1483e4a1b3701d0956ff2b2ca5
Gaba Bartnicka <bartnicka.g@gmail.com>
2020-06-22 23:38:12 +0200

    more data generated
    Signed-off-by: Gaba Bartnicka <bartnicka.g@gmail.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #/Users/gaba/Library/Java/JavaVirtualMachines/openjdk-14.0.1/Contents/Home/bin/java -jar  ./build/libs/blockchainsimulator-0.0.1.jar
- /Users/gaba/Library/Java/JavaVirtualMachines/openjdk-14.0.1/Contents/Home/bin/java -jar  ./build/libs/blockchainsimulator-0.0.1.jar --spring.rsocket.server.port=8083
+/Users/gaba/Library/Java/JavaVirtualMachines/openjdk-14.0.1/Contents/Home/bin/java -jar ./build/libs/blockchainsimulator-0.0.1.jar --spring.rsocket.server.port=8083
